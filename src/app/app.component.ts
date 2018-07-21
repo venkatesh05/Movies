ce5e24c24152b7a44ceae6aba6e0b6ffb1874204
Meagha <Meagha@Meaghas-Mac-mini.local>
2018-07-21 09:55:22 +0530

    Menu changes done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, NavController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
@@ -10,6 +10,7 @@ import { GoogleplayTabsPage } from '../pages/googleplay-tabs/googleplay-tabs';
 })
 export class MyApp {
   rootPage:any = GoogleplayTabsPage;
+  @ViewChild('nav') nav : NavController
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
